09ab40a4fd74131d0750900eb1b64b1fb22608a4
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2024-09-30 13:16:47 +0000

    task 1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -4,8 +4,8 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    let taskBlock = true;
-     taskBlock = false;
+     let task = true;
+     task2 = false;
   }
 
   return [task, task2];
